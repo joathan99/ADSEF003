ac6ad5bef6b103ea099d6fcd2a52b00c3a68e8c1
joathan99 <jonathans.r58@gmail.com>
2022-01-29 12:35:03 +0100

    ControladorPersonaje
    Se le ha añadido al personaje principal un controlador de movimiento

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+
+    public float speed = 10f;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        if (hor != 0.0f || ver != 0.0f)
+        {
+            Vector3 dir = transform.forward * ver + transform.right * hor;
+            rigidbody.MovePosition(transform.position + dir * speed * Time.deltaTime);
+        }
+    }
+}
